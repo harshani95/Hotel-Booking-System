48a2cc4c946c1f18d74177c03f83b72d21dc5a64
harshani <ruwanthikapemarathna@gmail.com>
2025-06-13 18:53:49 +0530

    Completed Hero Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import { useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
 
-  return <>{!isOwnerPath && <Navbar />}</>;
+  return (
+    <>
+      {!isOwnerPath && <Navbar />}
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
