07e0fc71ea5a0f4ac2e5478dfceea576677ce70c
harshani <ruwanthikapemarathna@gmail.com>
2025-06-23 17:14:45 +0530

    Created NewsLetter component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import FeaturedDestination from "../components/FeaturedDestination";
 import Hero from "../components/Hero";
+import NewsLetter from "../components/NewsLetter";
 import Testimonial from "../components/Testimonial";
 
 const Home = () => (
@@ -9,6 +10,7 @@ const Home = () => (
     <FeaturedDestination />
     <ExclusiveOffers />
     <Testimonial />
+    <NewsLetter />
   </>
 );
 
