58a4584895d54ec3b25dd3552917c0f85d39d6b9
harshani <ruwanthikapemarathna@gmail.com>
2025-06-26 11:51:13 +0530

    Implemented All Rooms Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,23 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <>
-      {!isOwnerPath && <Navbar />}
       <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        {!isOwnerPath && <Navbar />}
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<AllRooms />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
